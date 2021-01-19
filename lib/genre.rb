66190b28d171df769ccdc35e79315d1021fa58d6
Dayne Dewar <daynedewar@yahoo.com>
2021-01-18 22:59:35 -0500

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,21 @@
+class Genre 
+    @@all = []
+    attr_accessor :name
+    
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end
 
+    def self.all 
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        songs.collect {|song| song.artist}
+    end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,25 @@
+class Patient
+    @@all = []
+    attr_accessor :name
 
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select {|appt| appt.patient == self}
+    end
+
+    def doctors
+        appointments.collect {|appt| appt.doctor}
+    end
+end
